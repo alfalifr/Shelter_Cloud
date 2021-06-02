85cf5b9e0b5280fd1f97575815eb272b05b59e6b
zailbreck <no081574327482@gmail.com>
2021-06-02 12:13:17 +0000

    Delete File

--- API/dbConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost","shelter_dev","dev_shelter","shelter_caps");
-
-if ($conn -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $conn -> connect_error;
-  exit();
-}
-?>
