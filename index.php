fd4cb8193d1aa812770c7224e34a8f5b6a0f5c48
zailbreck <avicii.mn@gmail.com>
2021-06-07 01:23:03 +0700

    Telegram

--- index.php
+++ index.php
@@ -6,6 +6,26 @@ if(!isset($_SESSION)){
 if(!isset($_SESSION['PHPSESSID'])){
   header('location: login.php');
 }
+
+$API = "http://35.240.165.229/API/v1/shelter_api.php";
+if(!empty($_POST)){
+    $data = array(
+        "_feedback" => true,
+        "from" => $_POST["userreport"],
+        "type" => $_POST["state"],
+        "msg" => $_POST["message"]
+    );
+    $json_enc = json_encode($data);
+    
+    $ch = curl_init($API);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_enc);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+    echo "<script>if(!alert('Laporan Anda Terkirim')){ window.location.replace('index.php'); }</script>";
+    curl_close($ch);
+}
+
 ?>
 
 <!doctype html>
@@ -30,20 +50,19 @@ if(!isset($_SESSION['PHPSESSID'])){
             <div class="row h-100 justify-content-center align-items-center">
                 <div class="col-10 col-md-8 col-lg-6">
 					<!-- Form -->
-                	<form class="form-example" method="POST" action="login.php">
+                	<form class="form-example" method="POST" action="index.php">
                 		<h1>Shelter Feedback Form</h1>
                 		<p class="description">Being people helper by reveal disaster.</p>
-                    <input type="hidden" id="_authType" name="_authType" value="_login">
                 		
                     <!-- Input fields -->
                 		<div class="form-group">
-                			<input type="text" class="form-control" placeholder="E-Mail" readonly value="<?= $_SESSION['PHPSESSID'] ?>">
+                			<input type="text" class="form-control" name="userreport" placeholder="E-Mail" readonly value="<?= $_SESSION['PHPSESSID'] ?>">
                 		</div>
                     <div class="input-group mb-3">
                       <div class="input-group-prepend">
                         <label class="input-group-text" for="inputGroupSelect01">State</label>
                       </div>
-                      <select class="custom-select" id="inputGroupSelect01">
+                      <select class="custom-select" id="inputGroupSelect01" name="state">
                         <option selected>Choose...</option>
                         <option value="Feedback">Feedback</option>
                         <option value="Floods">Floods</option>
@@ -57,7 +76,7 @@ if(!isset($_SESSION['PHPSESSID'])){
                       <div class="input-group-prepend">
                         <span class="input-group-text">Message</span>
                       </div>
-                      <textarea class="form-control" aria-label="With textarea" style="height: 200px"></textarea>
+                      <textarea class="form-control" aria-label="With textarea" style="height: 200px" name="message"></textarea>
                     </div>
 
                     <div class="input-group mb-4">
@@ -83,3 +102,19 @@ if(!isset($_SESSION['PHPSESSID'])){
         <script src="assets/js/scripts.js"></script>
     </body>
 </html>
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Laporan Terkirim</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Loporan Terkirim. admin akan membalas laporan anda!
+      </div>
+    </div>
+  </div>
+</div>
